d70705b7d3fda2d9dff4ea273ca5eda240861d02
Mikhail <MIkovityy@gmail.com>
2020-02-20 00:00:29 +0300

    Initial Commit

---
+++ Homework 12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 12
+//
+//  Created by user on 2/20/20.
+//  Copyright © 2020 Mike Okovityy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
